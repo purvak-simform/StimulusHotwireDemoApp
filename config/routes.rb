a1ac2eebbdc988c10e2e345e6eea6e9e067a0aa8
PurvaK <purva.k@simformsolutions.com>
2023-01-18 12:39:43 +0530

    Devise, Turbo-drive, turbo-frame added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
   get 'home/index'
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
